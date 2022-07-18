67038c4242d9fc9a2a3c1c10a843c26f3ffdcebe
Danillo Ilggner <danilloilggner@gmail.com>
2022-07-18 15:57:41 -0300

    feat: criar projeto flutter

---
+++ android/app/src/main/kotlin/com/dnn/correio_elegante_devchallenge/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dnn.correio_elegante_devchallenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/cores.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const corRosaEscuro = Color(0xB73058);
+const corRosaClaro = Color(0xE06C88);
+const corCinza = Color(0x8D8D8D);
+const corCinzaEscuro = Color(0x2F394B);
+const corCinzaClaro = Color(0x9CABC2);
+const corFonte = Color(0xE7DFDD);
+final textStylePoppins = GoogleFonts.poppins();
